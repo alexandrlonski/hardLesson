50c634c4086fae3d96ae75279d67ff0f6d4bb7c8
Alexandr <aleksandr.lonski.86@gmail.com>
2020-10-13 12:18:13 +0300

    Усложнённое задание lesson №4

--- hard_lessons/script.js
+++ hard_lessons/script.js
@@ -1,50 +1,16 @@
-// Пункт№1
 
-let lang;
-langRu = ['Пн','Вт','СР','Чт','Пт','Сб','Вс'];
-langEn = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+function test(str) {
+  if (typeof str !== 'string') {
+    alert('аргумент не строка');
+    return;
+  }
 
-lang = prompt('Выберите язык', 'ru/en');
+  str = str.trim(); 
+  return str.length > 30 ? str.slice(0, 30) + '...' : str;
+};
 
-// Через if
-
-if (lang === 'ru') {
-  console.log(langRu);
-} else if (lang === 'en') {
-  console.log(langEn);
-}
-
-// Через Switch
-
-switch (lang) {
-  case 'ru':
-    console.log(langRu);
-    break
-  case 'en':
-    console.log(langEn);
-    break
-  default:
-    alert( "Повторите попытку" );  
-}
-
-// Через многомерный массив
-
-let langDay = [];
-langDay['ru'] = ['Пн','Вт','СР','Чт','Пт','Сб','Вс'];
-langDay['en'] = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-
-console.log(langDay[lang]);
+alert(test(prompt('Напишите предложение', '')));
+// alert(test(+prompt('Напишите предложение', '')));
 
 
-// Пункт№2
 
-let namePerson;
-
-namePerson = prompt('Введите своё имя', '');
-
-let whoAreYou = (namePerson.toLowerCase() === 'максим') ? 'преподаватель' :
-  (namePerson.toLowerCase() === 'артём') ? 'директор' :
-  'студент';
-
-alert(whoAreYou);
-console.log(whoAreYou);

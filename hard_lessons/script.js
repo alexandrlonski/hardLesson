b380f82121f1b796c51888cac56641ea678c3470
Alexandr <aleksandr.lonski.86@gmail.com>
2020-10-15 08:41:15 +0300

    Ещё один способ lesson04

--- hard_lessons/script.js
+++ hard_lessons/script.js
@@ -1,16 +1,26 @@
 
 function checkString(str) {
-  if (typeof str !== 'string') {
-    alert('аргумент не строка');
-    return;
+  if (typeof(str) === 'number' ) {
+    alert('вы ввели цифры');
+    console.log(str);
+  } else if ( str === "" ) {
+    alert('пустая строка');
+    console.log(str);
+  } else if ( str == null ) {
+    alert('null');
+    console.log(str);
+  } else {  
+    alert('вы ввели всё верно');
+    str = str.trim(); 
+    return str.length > 30 ? str.slice(0, 30) + '...' : str; 
+    
   }
-
-  str = str.trim(); 
-  return str.length > 30 ? str.slice(0, 30) + '...' : str;
+  
 };
+console.log(checkString(""));
 
-alert(checkString(prompt('Напишите предложение', '')));
-// alert(test(+prompt('Напишите предложение', '')));
+
+
 
 
 

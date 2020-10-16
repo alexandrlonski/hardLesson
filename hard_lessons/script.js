d9cbad5c18d8267dd2b446d690a0e99abcb920a6
Alexandr <aleksandr.lonski.86@gmail.com>
2020-10-17 00:10:04 +0300

    задание №5

--- hard_lessons/script.js
+++ hard_lessons/script.js
@@ -1,26 +1,27 @@
 
-function checkString(str) {
-  if (typeof(str) === 'number' ) {
-    alert('вы ввели цифры');
-    console.log(str);
-  } else if ( str === "" ) {
-    alert('пустая строка');
-    console.log(str);
-  } else if ( str == null ) {
-    alert('null');
-    console.log(str);
-  } else {  
-    alert('вы ввели всё верно');
-    str = str.trim(); 
-    return str.length > 30 ? str.slice(0, 30) + '...' : str; 
-    
-  }
-  
-};
-console.log(checkString(""));
+let arr = [
+  '234',
+  '348', 
+  '458', 
+  '789',
+  '257',
+  '589',
+  '439'
+];
 
+// arr.forEach((item) => {
+//   if (item.startsWith('2') || item.startsWith('4')) {
+//     console.log(item);
+//   }
+// });
 
+ for (let i = 0; i < arr.length; ++i) {
+   if (arr[i].substr(0, 1) == 2 || arr[i].substr(0, 1) == 4 ){
+     console.log(arr[i]);
+   }
+ }
 
-
-
-
+for (let i = 1; i <= 100; ++i) {
+  let arr = [1, i]
+  console.log(`${i} Делители этого числа: ${arr[0]} и ${arr[1]}`);
+}

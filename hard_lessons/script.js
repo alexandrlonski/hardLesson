458833e19d06fa7606cc1f7c3ad8755d80bca6d2
Alexandr <aleksandr.lonski.86@gmail.com>
2020-10-18 20:51:02 +0300

    Выполил усложнённое задание №7

--- hard_lessons/script.js
+++ hard_lessons/script.js
@@ -1,35 +1,10 @@
 
-let arr = [
-  '234',
-  '348', 
-  '458', 
-  '789',
-  '257',
-  '589',
-  '439'
-];
 
-// arr.forEach((item) => {
-//   if (item.startsWith('2') || item.startsWith('4')) {
-//     console.log(item);
-//   }
-// });
-
- for (let i = 0; i < arr.length; ++i) {
-   if (arr[i].substr(0, 1) == 2 || arr[i].substr(0, 1) == 4 ){
-     console.log(arr[i]);
-   }
- }
-
-let n = 100;
-
-firstFor:
-for ( let i = 2; i <= n; i++) { 
-
-  for (let j = 2; j < i; j++) { 
-    if (i % j == 0) continue firstFor; 
-  }
+let week = [ "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "<i>Суббота</i>", "<i>Воскресенье</i>" ];
+    
+let today = (6 + new Date().getDay()) % 7;
+week[today] = `<b>${ week[today] }</b>`;
   
-  let arr = [1, i];
-  console.log(`${i} Делитель этого числа: ${arr[0]} и ${arr[1]}`);
-}
+document.body.insertAdjacentHTML("afterBegin", week.join("<br>"));
+
+

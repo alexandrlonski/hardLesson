492c7c63cb739481aa77a860d91f864cbf489db8
Alexandr <aleksandr.lonski.86@gmail.com>
2020-10-21 01:03:55 +0300

    Выполнил задание №9

--- hard_lessons/script.js
+++ hard_lessons/script.js
@@ -1,10 +1,42 @@
 
+function add(a) {
+if (a < 10) {
+  a = '0' + a};
+  return a;
+};  
 
-let week = [ "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "<i>Суббота</i>", "<i>Воскресенье</i>" ];
-    
-let today = (6 + new Date().getDay()) % 7;
-week[today] = `<b>${ week[today] }</b>`;
-  
-document.body.insertAdjacentHTML("afterBegin", week.join("<br>"));
+
+setInterval( function(){
+let Data = new Date(), 
+Year = Data.getFullYear(),
+NumDay = Data.getDate(),
+Hour = Data.getHours(),
+Minutes = Data.getMinutes(),
+Seconds = setInterval(Data.getSeconds()),
+week = [ "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "<i>Суббота</i>", "<i>Воскресенье</i>" ],
+today = (6 + new Date().getDay()) % 7,
+Day = week[today];
+nameMonth = ["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря" ],
+Month = nameMonth[Data.getMonth()],
+textHour = function() {
+   if(Hour > 1 && Hour < 5 || Hour >= 22 && Hour <= 24) {
+    return "часа";
+  } else if(Hour >= 5 && Hour <= 20 || Hour === 0) {
+    return "часов";
+  } else {
+    return "час";
+  }; 
+}; 
+
+
+document.getElementById('dateOne').innerHTML = (`Cегодня ${Day}, ${NumDay} ${Month} ${Year} года, ${Hour} ${textHour()} ${Minutes} минуты ${Seconds} секунды` );
+document.getElementById('dateTwo').innerHTML =  (`${add(NumDay)}.${add(Data.getMonth())}.${Year} - ${add(Hour)}.${add(Minutes)}.${add(Seconds)}`);
+}, 1000);
+
+
+
+// document.write(`Cегодня ${Day}, ${NumDay} ${Month} ${Year} года, ${Hour} ${textHour()} ${Minutes} минуты ${Seconds} секунды` );
+// document.write(`${add(NumDay)}.${add(Data.getMonth())}.${Year} - ${add(Hour)}.${add(Minutes)}.${add(Seconds)}`);
+
 
 
